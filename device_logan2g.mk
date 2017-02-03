cd744b600ddc89f895834d8f9f2b366023393b5d
versusx <armannet123@mail.ru>
2017-02-03 16:02:34 +0500

    update

--- device_logan2g.mk
+++ device_logan2g.mk
@@ -96,8 +96,7 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
-
+    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml 
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
